3153c13e10bbba72309e91b60d7a56fbd188739d
jpeter88 <jrpeterson@unomaha.edu>
2020-10-15 09:55:42 -0400

    updated print format

--- Files/TextModelCore.py
+++ Files/TextModelCore.py
@@ -21,10 +21,22 @@ class TextModelCoreClass:
     return TextModelClass.classificationModel(self, terms)
 
 if __name__ == "__main__":
+  #from TextModelCore import TextModelCoreClass
   tm = TextModelCoreClass()
+
   tm_getFilePath = tm.getFilePath('..\\Data\\financial_news.csv')
-  #tm_getSentimentPlot = tm.getSentimentPlot(tm_getFilePath)
+  print(tm_getFilePath)
+
+  tm_getSentimentPlot = tm.getSentimentPlot(tm_getFilePath)
+
   tm_getBagOfWords = tm.getBagOfWords(tm_getFilePath)
+  print(tm_getBagOfWords)
+
   tm_getMakeModel = tm.getMakeModel(tm_getFilePath,tm_getBagOfWords)
+  print(tm_getMakeModel)
+
   tm_getTermFreqIDF = tm.getTermFreqIDF(tm_getFilePath)
+  print(tm_getTermFreqIDF)
+
   tm_getClassificationModel = tm.getClassificationModel(tm_getTermFreqIDF)
+  print(tm_getClassificationModel)
--- Files/TextParserCore.py
+++ Files/TextParserCore.py
@@ -1,31 +1,44 @@
 from TextParser import TextParserClass
-#source NLPenv/Scripts/activate
 
 class TextParserCoreClass:
 
   def getFilePath(self, filepath):
-    return FileParser.fileRead(self, filepath)
+    return TextParserClass.fileRead(self, filepath)
 
   def getFreqDist(self, files):
-    return FileParser.findFreqDist(self, files)
+    return TextParserClass.findFreqDist(self, files)
 
   def getStopWords(self, files):
-    return FileParser.removeStopWords(self, files)
+    return TextParserClass.removeStopWords(self, files)
 
   def getWordStems(self, files):
-    return FileParser.wordStemmer(self, files)
+    return TextParserClass.wordStemmer(self, files)
 
   def getWordLems(self, files):
-    return FileParser.wordLemmatizer(self, files)
+    return TextParserClass.wordLemmatizer(self, files)
 
   def getPartSpeechTags(self, files):
-    return FileParser.speechTagger(self, files)
+    return TextParserClass.speechTagger(self, files)
 
 if __name__ == "__main__":
+    #source NLPenv/Scripts/activate
+    #from TextParserCore import TextParserCoreClass
     cr = TextParserCoreClass()
+
     cr_getFilePath = cr.getFilePath('..\\Data\\financial_news.csv')
+    print(cr_getFilePath)
+
     cr_getFreqDist = cr.getFreqDist(cr_getFilePath)
+    print(cr_getFreqDist)
+
     cr_getStopwords = cr.getStopWords(cr_getFilePath)
+    print(cr_getStopwords)
+
     cr_getWordStems = cr.getWordStems(cr_getStopwords)
+    print(cr_getWordStems)
+
     cr_getWordLems = cr.getWordLems(cr_getStopwords)
+    print(cr_getWordLems)
+
     cr_getPartSpeechTags = cr.getPartSpeechTags(cr_getStopwords)
+    print(cr_getPartSpeechTags)
